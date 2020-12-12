6b44c678b591a3f36416aaa156879ca1bb2530fa
ictechgy <ictechgy@gmail.com>
2020-12-12 23:09:55 +0900

    filtering iOS버전 만들기 시작

---
+++ Filtering/Controllers/SearchResultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SearchResultViewController.swift
+//  Filtering
+//
+//  Created by JINHONG AN on 2020/12/12.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+    
+    //ViewController에서 입력한 검색 내용
+    var searchContent: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Filtering/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Filtering
+//
+//  Created by JINHONG AN on 2020/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBOutlet weak var showMaskAuthorizedList: UIButton!
+    
+    @IBAction func tapOutsideView(_ sender: UITapGestureRecognizer){
+        self.view.endEditing(true)
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let sender = sender as? UIButton else {
+            return
+        }
+        
+        switch sender {
+        case searchButton:
+            if searchBar.text?.count == 0 {     //아무 내용도 입력하지 않고 검색 버튼을 누른 경우
+                let alertController: UIAlertController = UIAlertController(title: "입력한 내용 없음", message: "의약외품 이름을 입력하세요", preferredStyle: .alert)
+                
+                let okAlertAction: UIAlertAction = UIAlertAction(title: "ok", style: .default, handler: nil)
+                alertController.addAction(okAlertAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            
+            //여기서 작동방식을 선택할 수 있다. 검색을 누른다면 검색결과를 먼저 가져온 뒤에 다음화면으로 넘길지, 아니면 다음화면에 가서 검색을 시작할지..
+
+            guard let nextViewController = segue.destination as? SearchResultViewController else {
+                return
+            }
+            nextViewController.searchContent = searchBar.text
+            
+        case showMaskAuthorizedList:
+            guard let nextViewController = segue.destination as? MaskAuthorizedListViewController else {
+                return
+            }
+        default:
+            return
+        }
+    }
+}
+
